c4be794e4e0cb2949dbdb56e1e184d02dcd01ce9
Anderson Lima <andclima@gmail.com>
2020-09-16 19:00:25 -0300

    Persistência TipoFormacao parcial

--- src/main/java/com/github/andclima/eskillo/EskilloApplication.java
+++ src/main/java/com/github/andclima/eskillo/EskilloApplication.java
@@ -1,12 +1,5 @@
 package com.github.andclima.eskillo;
 
-import com.github.andclima.eskillo.model.AreaEstudo;
-import com.github.andclima.eskillo.model.Experiencia;
-import com.github.andclima.eskillo.model.Formacao;
-import com.github.andclima.eskillo.model.Localidade;
-import com.github.andclima.eskillo.model.Perfil;
-import com.github.andclima.eskillo.model.TipoFormacao;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,43 +14,43 @@ public class EskilloApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		AreaEstudo area1 = new AreaEstudo();
-		area1.setId(1l);
-		area1.setDescricao("Engenharia de Software");
+		// AreaEstudo area1 = new AreaEstudo();
+		// area1.setId(1l);
+		// area1.setDescricao("Engenharia de Software");
 		
-		AreaEstudo area2 = new AreaEstudo(2l, "Gastronomia");
-		System.out.println(area2.toString());
+		// AreaEstudo area2 = new AreaEstudo(2l, "Gastronomia");
+		// System.out.println(area2.toString());
 
-		TipoFormacao tipo1 = new TipoFormacao();
-		tipo1.setId(1l);
-		tipo1.setDescricao("Bacharelado");
-		System.out.println(tipo1.toString());
+		// TipoFormacao tipo1 = new TipoFormacao();
+		// tipo1.setId(1l);
+		// tipo1.setDescricao("Bacharelado");
+		// System.out.println(tipo1.toString());
 
-		Perfil perfilAnderson = new Perfil();
-		perfilAnderson.setId(10l);
-		perfilAnderson.setNome("Anderson");
-		perfilAnderson.setCargoAtual("Professor");
-		perfilAnderson.setEmail("anderson.lima@grupocev.com");
+		// Perfil perfilAnderson = new Perfil();
+		// perfilAnderson.setId(10l);
+		// perfilAnderson.setNome("Anderson");
+		// perfilAnderson.setCargoAtual("Professor");
+		// perfilAnderson.setEmail("anderson.lima@grupocev.com");
 
-		Formacao formacao1 = new Formacao();
-		formacao1.setId(1l);
-		formacao1.setTipoFormacao(tipo1);
-		formacao1.setAreaEstudo(area1);
-		formacao1.setAnoInicio(2020);
-		formacao1.setAnoFim(2022);
-		formacao1.setNomeInstituicao("iCEV");
-		formacao1.setEmAndamento(true);
+		// Formacao formacao1 = new Formacao();
+		// formacao1.setId(1l);
+		// formacao1.setTipoFormacao(tipo1);
+		// formacao1.setAreaEstudo(area1);
+		// formacao1.setAnoInicio(2020);
+		// formacao1.setAnoFim(2022);
+		// formacao1.setNomeInstituicao("iCEV");
+		// formacao1.setEmAndamento(true);
 
-		perfilAnderson.getFormacoes().add(formacao1);
+		// perfilAnderson.getFormacoes().add(formacao1);
 
-		Experiencia exp1 = new Experiencia();
-		exp1.setId(1l);
-		exp1.setCargo("Professor");
-		exp1.setNomeEmpresa("iCEV");
-		exp1.setLocalidade(new Localidade(1l, "Teresina", "PI"));
-		exp1.setOcupacaoAtual(true);
+		// Experiencia exp1 = new Experiencia();
+		// exp1.setId(1l);
+		// exp1.setCargo("Professor");
+		// exp1.setNomeEmpresa("iCEV");
+		// exp1.setLocalidade(new Localidade(1l, "Teresina", "PI"));
+		// exp1.setOcupacaoAtual(true);
 
-		perfilAnderson.getExperiencias().add(exp1);
+		// perfilAnderson.getExperiencias().add(exp1);
 
 	}
 
---
+++ src/main/java/com/github/andclima/eskillo/services/TipoFormacaoService.java
@@ -0,0 +1,34 @@
+package com.github.andclima.eskillo.services;
+
+import java.util.List;
+
+import com.github.andclima.eskillo.model.TipoFormacao;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TipoFormacaoService {
+
+
+    public List<TipoFormacao> lista() {
+        
+        return null;
+    }
+
+    public TipoFormacao busca(Long id) {
+
+        return null;
+    }
+
+    public void adiciona(TipoFormacao tipoFormacao) {
+        
+    }
+    
+    public void atualiza(TipoFormacao tipoFormacao) {
+        
+    }
+
+    public void remove(TipoFormacao tipoFormacao) {
+        
+    }
+}
